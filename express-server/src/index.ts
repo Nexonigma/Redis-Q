e42c1c8b6b73164d5bda219865614d74726bb2bc
arjimo <monishmukhari@gmail.com>
2024-07-07 13:04:41 +0530

    Added worker
    Co-authored-by: monish-mukhari manishmukherjee91@gmail.com

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,18 +1,38 @@
-import express from 'express';
+import express from "express";
 import { createClient } from "redis";
 
 const app = express();
 app.use(express.json());
 
 const client = createClient();
-client.connect();
+client.on('error', (err) => console.log('Redis Client Error', err));
 
-app.post("/submit", (req, res) => {
-    const { problemId, userId, code, language } = req.body;
-    client.lPush("submissions", JSON.stringify({ problemId, userId, code, language }));
-    res.json({
-        message: "Submission received!"
-    })
+app.post("/submit", async (req, res) => {
+    const problemId = req.body.problemId;
+    const code = req.body.code;
+    const language = req.body.language;
+
+    try {
+        const response = await client.lPush("problems", JSON.stringify({ code, language, problemId }));
+        console.log(response);
+        res.status(200).send("Submission received and stored.");
+    } catch (error) {
+        console.error("Redis error:", error);
+        res.status(500).send("Failed to store submission.");
+    }
 });
 
-app.listen(3000);
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("Connected to Redis");
+
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startServer();
